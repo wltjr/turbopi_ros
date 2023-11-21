f597bff26636832ce9a38fb7b89e83b2a1102cb7
William L. Thomson Jr. <wlt@o-sinc.com>
2023-11-21 00:41:14 -0500

    launch2/turbopi_ros.launch.py: Cleaned up from example code

--- launch2/turbopi_ros.launch.py
+++ launch2/turbopi_ros.launch.py
@@ -3,8 +3,6 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
 import xacro
@@ -14,13 +12,12 @@ def generate_launch_description():
 
     pkg_name = 'turbopi_ros'
     filename = 'turbopi.urdf.xacro'
-    use_sim_time = LaunchConfiguration('use_sim_time')
 
     pkg_path = os.path.join(get_package_share_directory(pkg_name))
     xacro_file = os.path.join(pkg_path,'description',filename)
     robot_description_config = xacro.process_file(xacro_file)
 
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    params = {'robot_description': robot_description_config.toxml()}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -28,12 +25,6 @@ def generate_launch_description():
         parameters=[params]
     )
 
-
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-
         node_robot_state_publisher
     ])
