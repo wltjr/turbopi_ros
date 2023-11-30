76a6e4f6ab94f67784ecff86109a774b5995a39e
William L. Thomson Jr. <wlt@o-sinc.com>
2023-11-29 21:03:40 -0500

    launch2/turbopi_ros.launch.py: Modified to start controller manager
    - Start contoller manager with robot description (deprecated)
    - Start diff drive controller and join state broadcaster

--- launch2/turbopi_ros.launch.py
+++ launch2/turbopi_ros.launch.py
@@ -3,6 +3,9 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
 
 import xacro
@@ -14,17 +17,57 @@ def generate_launch_description():
     filename = 'turbopi.urdf.xacro'
 
     pkg_path = os.path.join(get_package_share_directory(pkg_name))
+    controller_params = os.path.join(pkg_path, 'config', 'turbopi_controllers.yaml')
     xacro_file = os.path.join(pkg_path,'description',filename)
     robot_description_config = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': robot_description_config.toxml()}
 
-    params = {'robot_description': robot_description_config.toxml()}
+    controller_manager = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, controller_params],
+        output='both',
+    )
+
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
+        output='both',
+        parameters=[robot_description],
+        remappings=[
+            ("/turbopi_controller/cmd_vel_unstamped", "/cmd_vel"),
+        ],
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller", "-c", "/controller_manager"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "-c", "/controller_manager"],
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_broad_spawner,
+            on_exit=[diff_drive_spawner],
+        )
     )
 
     return LaunchDescription([
-        node_robot_state_publisher
+        controller_manager,
+        node_robot_state_publisher,
+        delayed_joint_broad_spawner,
+        delayed_diff_drive_spawner,
     ])
