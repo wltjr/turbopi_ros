d27719090c80a599d3dff8eb1d8fb67df8e774ff
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-02 20:55:57 -0500

    include/turbopi_hardware_interface.hpp: Initial hardware interface
    - Based on code from ros2_controls demos example 2 diffbot
      https://github.com/ros-controls/ros2_control_demos/tree/master/example_2

---
+++ include/turbopi_hardware_interface.hpp
@@ -0,0 +1,83 @@
+// Copyright 2021 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TURBOPI__TURBOPI_HARDWARE_INTERFACE_HPP_
+#define TURBOPI__TURBOPI_HARDWARE_INTERFACE_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "rclcpp/clock.hpp"
+#include "rclcpp/duration.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/time.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+
+#include "visibility_control.h"
+
+extern const auto CLASS_NAME = "TurboPiSystemHardware";
+
+namespace turbopi_hardware_interface
+{
+    class TurboPiSystemHardware : public hardware_interface::SystemInterface
+    {
+    public:
+        RCLCPP_SHARED_PTR_DEFINITIONS(TurboPiSystemHardware)
+
+        TURBOPI_PUBLIC
+        hardware_interface::CallbackReturn on_init(
+            const hardware_interface::HardwareInfo &info) override;
+
+        TURBOPI_PUBLIC
+        std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+
+        TURBOPI_PUBLIC
+        std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+
+        TURBOPI_PUBLIC
+        hardware_interface::CallbackReturn on_activate(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        TURBOPI_PUBLIC
+        hardware_interface::CallbackReturn on_deactivate(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        TURBOPI_PUBLIC
+        hardware_interface::return_type read(
+            const rclcpp::Time &time, const rclcpp::Duration &period) override;
+
+        TURBOPI_PUBLIC
+        hardware_interface::return_type write(
+            const rclcpp::Time &time, const rclcpp::Duration &period) override;
+
+    private:
+        // params
+        double hw_start_sec_;
+        double hw_stop_sec_;
+
+        // store command for simulated robot
+        std::vector<double> hw_commands_;
+        std::vector<double> hw_positions_;
+        std::vector<double> hw_velocities_;
+    };
+
+}
+
+#endif
