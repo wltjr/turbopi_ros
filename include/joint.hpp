9af604272038f2463446322e0f71d0fe32e4f33c
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-12 21:17:13 -0500

    include/joint.hpp: Cleanup header group methods and variables

--- include/joint.hpp
+++ include/joint.hpp
@@ -20,33 +20,36 @@ namespace turbopi
 		private:
 			uint8_t _motorId = 0;
 			uint8_t _actuatorType = 0;
-			int8_t _getSlaveAddress();
 			uint8_t _minServoValue = 0;
 			uint8_t _maxServoValue = 75;
-			double _previousEffort;
-			double _filterAngle(double angle);
 			int _angleReads = 0;
 			static const int _filterPrevious = 3;
+			double _previousEffort;
 			double _previousAngles[_filterPrevious];
 
-			void _prepareI2CWrite(int8_t result[2], double effort);
+			double _filterAngle(double angle);
+			int8_t _getSlaveAddress();
 			void _prepareI2CRead(int8_t result[2]);
+			void _prepareI2CWrite(int8_t result[2], double effort);
+
 		public:
-			std::string name;
-			Joint();
-			Joint(uint8_t motorId);
-			~Joint();
 			double sensorResolution = 1024;
 			double angleOffset = 0;
 			double readRatio = 1;
+			std::string name;
+
+			Joint();
+			Joint(uint8_t motorId);
+			~Joint();
+
+			void actuate(double effort, uint8_t duration);
+			int getActuatorType();
 			uint8_t getMotorId();
+			double getPreviousEffort();
+			double readAngle();
 			void setMotorId(uint8_t motorId);
 			void setActuatorType(uint8_t actuatorType);
 			void setServoLimits(uint8_t minValue, uint8_t maxValue);
-			int getActuatorType();
-			double getPreviousEffort();
-			void actuate(double effort, uint8_t duration);
-			double readAngle();
 	};
 }
 
