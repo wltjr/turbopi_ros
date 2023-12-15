65a1e2fae8b73cc7278659d54b8364baeb04ea8f
William L. Thomson Jr. <wlt@o-sinc.com>
2023-12-14 23:22:55 -0500

    include/i2c.hpp: Removed unused variable and define

--- include/i2c.hpp
+++ include/i2c.hpp
@@ -3,8 +3,6 @@
 
 #include <inttypes.h>
 
-#define BUFFER_SIZE 1
-
 extern const char *CLASS_NAME;
 
 namespace turbopi
@@ -12,8 +10,6 @@ namespace turbopi
 	class I2C
 	{
 		public:
-			uint8_t dataBuffer[BUFFER_SIZE];
-
 			I2C(uint8_t bus, int8_t address);
 			virtual ~I2C();
 			uint8_t readBytes(int8_t registerNumber, uint8_t bufferSize, int8_t &position);
