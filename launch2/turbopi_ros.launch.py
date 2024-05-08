8326a5591cc14691aa18ecf2742d3179324dc76d
William L. Thomson Jr. <wlt@o-sinc.com>
2024-05-08 00:01:25 -0400

    launch2/turbopi_ros.launch.py: Modified to accept command line args, sim arg
    - Modified to accept command line arguments, starting with the sim argument
      to control mock or actual hardware, true for mock, default is false

--- launch2/turbopi_ros.launch.py
+++ launch2/turbopi_ros.launch.py
@@ -3,24 +3,34 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from launch.event_handlers import OnProcessStart
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, RegisterEventHandler
+from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch.launch_context import LaunchContext
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 
-import xacro
-
-
-def generate_launch_description():
+def launch_setup(context: LaunchContext):
 
     pkg_name = 'turbopi_ros'
     filename = 'turbopi.urdf.xacro'
 
     pkg_path = os.path.join(get_package_share_directory(pkg_name))
+    sim = eval(context.perform_substitution(LaunchConfiguration('sim')).title())
     controller_params = os.path.join(pkg_path, 'config', 'turbopi_controllers.yaml')
     xacro_file = os.path.join(pkg_path,'description',filename)
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_description = {'robot_description': robot_description_config.toxml()}
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            xacro_file,
+            " ",
+            "use_hardware:=",
+            "mock" if sim else "robot",
+            " ",
+        ]
+    )
+    robot_description = {'robot_description': robot_description_content}
 
     controller_manager = Node(
         package='controller_manager',
@@ -66,9 +76,24 @@ def generate_launch_description():
         )
     )
 
-    return LaunchDescription([
+    nodes = [
         controller_manager,
         node_robot_state_publisher,
         delayed_joint_broad_spawner,
         delayed_diff_drive_spawner,
-    ])
+    ]
+
+    return nodes
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "sim",
+            default_value="False",
+            description="Start with simulated mock hardware",
+        )
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
