135b78895d99d2e506c7c040dbe9d41cabea08b9
William L. Thomson Jr. <wlt@o-sinc.com>
2024-05-24 16:15:11 -0400

    src/sonar.cpp: Initial code for Sonar class for I2C sonar sensor

---
+++ src/sonar.cpp
@@ -0,0 +1,75 @@
+/** Copyright 2024 William L Thomson Jr <w@wltjr.com>
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ */
+
+#include "sonar.hpp"
+
+namespace turbopi
+{
+    Sonar::Sonar()
+    {
+    }
+ 
+    Sonar::Sonar(uint8_t i2c_dev, uint8_t i2c_address)
+    {
+        i2c_address_ = i2c_address;
+        I2C i2c = I2C(i2c_dev, i2c_address_);
+        i2c_ = &i2c;
+    }
+
+    Sonar::~Sonar()
+    {
+    }
+
+    int Sonar::getDistance()
+    {
+        uint8_t distance = 255;
+
+        uint8_t result = i2c_->readBytes(i2c_address_, 2, distance);
+        if (result != 1)
+		{
+            distance = 0;
+        }
+
+        return distance;
+    }
+
+    std::array<uint8_t, 3> Sonar::getPixelColor(uint8_t index)
+    {
+        if (index != 0 && index != 1)
+            return {0,0,0};
+        return {((pixels[index] >> 16) & 0xFF),
+                ((pixels[index] >> 8) & 0xFF),
+                (pixels[index] & 0xFF)};
+    }
+
+    uint8_t Sonar::setPixelColor(uint8_t index, uint8_t rgb)
+    {
+        if (index != 0 && index != 1)
+            return 0;
+
+        uint8_t start_reg = (index == 0) ? 3:6;
+		uint8_t data[2];
+
+        data[0] = start_reg;
+        data[1] = (uint8_t)(0xFF & (rgb >> 16));
+        uint8_t result = i2c_->writeData(i2c_address_, data);
+        data[0] = start_reg+1;
+        data[1] = (uint8_t)(0xFF & (rgb >> 8));
+        result = i2c_->writeData(i2c_address_, data);
+        data[0] = start_reg+2;
+        data[1] = (uint8_t)(0xFF & rgb);
+        result = i2c_->writeData(i2c_address_, data);
+        pixels[index] = rgb;
+
+        return result;
+    }
+
+    void Sonar::setRGBMode(uint8_t mode)
+    {
+        rgb_mode_ = mode;
+    }
+
+}
