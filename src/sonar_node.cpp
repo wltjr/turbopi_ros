2c43f4eba78ef605f5a0d115f0a2bbe43ab5a9eb
William L. Thomson Jr. <wlt@o-sinc.com>
2024-06-08 10:07:39 -0400

    src/sonar_node.cpp: Initial sonar node class exposes distance via /sonar
    - ROS 2 node to wrap the sonar class and expose distance via /sonar topic

---
+++ src/sonar_node.cpp
@@ -0,0 +1,65 @@
+/** Copyright 2024 William L Thomson Jr <w@wltjr.com>
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ */
+
+#include <memory>
+
+#include "sonar.hpp"
+#include "sonar_node.hpp"
+
+using namespace std::chrono_literals;
+
+namespace turbopi
+{
+
+    SonarNode::SonarNode(const rclcpp::NodeOptions &options)
+        : Node("sonar", options)
+    {
+        static Sonar sonar = Sonar(1, SONAR_ADDRESS);
+        sonar_ = &sonar;
+
+        // publish to /sonar topic
+        publisher_sonar_ =
+            this->create_publisher<sensor_msgs::msg::Range>("sonar",
+                rclcpp::QoS(rclcpp::KeepLast(1)).transient_local().reliable());
+
+        timer_sonar_ = 
+            this->create_wall_timer(500ms, std::bind(&SonarNode::callbackSonarPublisher, this));
+    }
+
+    void SonarNode::callbackSonarPublisher()
+    {
+        // Initializes with zeros by default.
+        auto msg = std::make_unique<sensor_msgs::msg::Range>();
+
+        msg->header.stamp = rclcpp::Time(0, 0, this->get_clock()->get_clock_type());
+        msg->header.frame_id = "sonar";
+        msg->radiation_type = sensor_msgs::msg::Range::ULTRASOUND;
+        msg->field_of_view = 0;
+        msg->min_range = 10;
+        msg->max_range = 5000;
+        msg->range = sonar_->getDistance();
+
+        publisher_sonar_->publish(std::move(msg));
+    }
+}
+
+/**
+ * @brief Node executable wrapper for the shared object
+ * 
+ * @param argc the count of arguments passed to the program on start
+ * @param argv the argument values passed to the program on start
+ * @return int program return code
+ */
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+
+    rclcpp::spin(std::make_unique<turbopi::SonarNode>(rclcpp::NodeOptions()));
+
+    rclcpp::shutdown();
+
+    return 0;
+}
