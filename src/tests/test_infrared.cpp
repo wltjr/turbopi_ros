d74010d2a6e71b74e02e217bbfad404594fcbb59
William L. Thomson Jr. <wlt@o-sinc.com>
2024-06-10 20:17:20 -0400

    src/tests/test_infrared.cpp: Initial test program for infrared sensor

---
+++ src/tests/test_infrared.cpp
@@ -0,0 +1,27 @@
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+
+#include "infrared.hpp"
+
+/**
+ * @brief Test program to test out infrared sensors
+ *
+ * @return int program return status/value
+ */
+int main()
+{
+    turbopi::Infrared infrared = turbopi::Infrared(1, INFRARED_ADDRESS);
+
+    while (true)
+    {
+        auto values = infrared.getValues();
+        std::cout << "Sensor 1 " << +values[0] 
+                << ", Sensor 2 " << +values[1] 
+                << ", Sensor 3 " << +values[2]  
+                << ", Sensor 4 " << +values[3] << std::endl << std::endl;
+                sleep(0.5);
+    }
+}
