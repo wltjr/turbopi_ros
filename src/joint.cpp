5e333daeb005e7553ad77ba567140dbcee1b4a2f
William L. Thomson Jr. <w@wltjr.com>
2025-01-03 10:11:35 -0400

    src/joint.cpp: Reformat, replace any tabs with spaces

--- src/joint.cpp
+++ src/joint.cpp
@@ -1,5 +1,5 @@
 /** Copyright 2024 William L Thomson Jr <w@wltjr.com>
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  */
@@ -14,62 +14,61 @@
 
 namespace turbopi
 {
-	Joint::Joint() = default;
+    Joint::Joint() = default;
 
-    Joint::Joint(uint8_t id, uint8_t type, I2C &i2c) :
-        i2c_(&i2c), id_(id), type_(type)
-	{
-	}
+    Joint::Joint(uint8_t id, uint8_t type, I2C &i2c) : i2c_(&i2c), id_(id), type_(type)
+    {
+    }
 
-	Joint::~Joint() = default;
+    Joint::~Joint() = default;
 
-	void Joint::setType(uint8_t type)
-	{
-		this->type_ = type;
-	}
+    void Joint::setType(uint8_t type)
+    {
+        this->type_ = type;
+    }
 
-	uint8_t Joint::getId()
-	{
-		return this->id_;
-	}
+    uint8_t Joint::getId()
+    {
+        return this->id_;
+    }
 
-	double Joint::getValue()
-	{
-		if (type_ == TYPE_MOTOR)
-		{
-			uint8_t position;
+    double Joint::getValue()
+    {
+        if (type_ == TYPE_MOTOR)
+        {
+            uint8_t position;
 
-			if (i2c_->readBytes(id_ - 1 + MOTOR_ADDRESS, 1, position))
-			{
-				return position;
-			}
-			else
-			{
-				RCLCPP_ERROR(rclcpp::get_logger(CLASS_NAME),
-							 "I2C Read Error during joint %s position read.",
+            if (i2c_->readBytes(id_ - 1 + MOTOR_ADDRESS, 1, position))
+            {
+                return position;
+            }
+            else
+            {
+                RCLCPP_ERROR(rclcpp::get_logger(CLASS_NAME),
+                             "I2C Read Error during joint %s position read.",
                              name.c_str());
-				return 0;
-			}
-		}
-		else if (type_ == TYPE_SERVO)
-		{
-			return _previousEffort;
-		}
-		else
-		{
-			return 0;
-		}
-	}
+                return 0;
+            }
+        }
+        else if (type_ == TYPE_SERVO)
+        {
+            return _previousEffort;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
-	void Joint::actuate(double effort, uint8_t /*duration = 1*/)
-	{
-		std::array<uint8_t, 2> data;
+    void Joint::actuate(double effort, uint8_t /*duration = 1*/)
+    {
+        std::array<uint8_t, 2> data;
 
-		if (type_ == TYPE_MOTOR)
-		{
+        if (type_ == TYPE_MOTOR)
+        {
             int8_t speed = 0;
 
-            if(floor(effort) != 0)
+            if (floor(effort) != 0)
             {
                 const int8_t LOW = 50;
 
@@ -79,28 +78,28 @@ namespace turbopi
                     speed = 100;
                 else if (speed < -100)
                     speed = -100;
-                else if(speed > 0 && speed < LOW)
+                else if (speed > 0 && speed < LOW)
                     speed = LOW;
-                else if(speed < 0 && speed > -LOW)
+                else if (speed < 0 && speed > -LOW)
                     speed = -LOW;
 
                 // invert speeds for right side
-                if(id_ & 1)
+                if (id_ & 1)
                     speed = -speed;
             }
 
-			data[0] = id_ - 1;
-			data[1] = speed;
+            data[0] = id_ - 1;
+            data[1] = speed;
 
-			uint8_t result = i2c_->writeData(MOTOR_ADDRESS, data);
+            uint8_t result = i2c_->writeData(MOTOR_ADDRESS, data);
             RCLCPP_DEBUG(rclcpp::get_logger(CLASS_NAME),
                          "write: %i; effort: %f; motor: %i, speed: %i",
                          result, effort, data[0], data[1]);
-		}
-		else if (type_ == TYPE_SERVO)
-		{
-			if (effort != _previousEffort)
-			{
+        }
+        else if (type_ == TYPE_SERVO)
+        {
+            if (effort != _previousEffort)
+            {
                 uint8_t angle = effort * 90 + 90;
                 uint8_t pulse = ((200 * angle) / 9) + 500;
                 std::array<uint8_t, 2> pulse_data;
@@ -118,34 +117,34 @@ namespace turbopi
                 pulse_data[2] = data[0];
                 pulse_data[3] = pulse;
 
-				uint8_t result = i2c_->writeData(CAMERA_ADDRESS, data);
+                uint8_t result = i2c_->writeData(CAMERA_ADDRESS, data);
                 RCLCPP_DEBUG(rclcpp::get_logger(CLASS_NAME),
                              "write: %i; effort: %f; joint %s max %i servo: %i angle: %i°",
                              result, effort, name.c_str(), max_, data[0], data[1]);
 
-				result = i2c_->writeData(SERVO_ADDRESS_CMD, pulse_data);
+                result = i2c_->writeData(SERVO_ADDRESS_CMD, pulse_data);
                 RCLCPP_DEBUG(rclcpp::get_logger(CLASS_NAME),
                              "write: %i; effort: %f; servo: %i time: %i pulse %i",
                              result, effort, pulse_data[2], pulse_data[1], pulse_data[3]);
-			}
-		}
+            }
+        }
 
-		_previousEffort = effort;
-	}
+        _previousEffort = effort;
+    }
 
-	void Joint::setLimits(uint8_t min, uint8_t max)
-	{
-		this->min_ = min;
-		this->max_ = max;
-	}
+    void Joint::setLimits(uint8_t min, uint8_t max)
+    {
+        this->min_ = min;
+        this->max_ = max;
+    }
 
-	double Joint::getPreviousEffort()
-	{
-		return this->_previousEffort;
-	}
+    double Joint::getPreviousEffort()
+    {
+        return this->_previousEffort;
+    }
 
-	int Joint::getType()
-	{
-		return type_;
-	}
+    int Joint::getType()
+    {
+        return type_;
+    }
 }
